d61c634aae2e3baa4c9731b374edc2ed5250418b
Arj09 <arjunkushwah100199@gmail.com>
2025-06-04 13:44:23 +0530

    add

--- Backend-Ev/server.js
+++ Backend-Ev/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const DBconnection = require('./dbConfig/dbConnection')
+const errorHandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config()
 const cors = require('cors')
 
@@ -15,6 +16,7 @@ app.use(cors())
 app.use(express.json());
 app.use("/api/station", require("./routers/stationRouter"))
 app.use("/api/user", require("./routers/userRouter"))
+app.use(errorHandler);
 
 app.listen(port, (req, res)=>{
     console.log(`server is running on port : ${port}`)
